8639af629a62508c7fe1ac66f9634f2625ee9eb2
segamorozov2013@yandex.ru <unukalhai@DESKTOP-136JS7H>
2023-04-05 13:29:57 +0300

    add cli.js

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+// @ts-ignore
+import readlineSync from 'readline-sync';
+
+const yourName = () => {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+};
+
+export { yourName };
