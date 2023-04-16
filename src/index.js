189c1526e029e97e47cb689052ab3be5e2c507d1
segamorozov2013@yandex.ru <unukalhai@DESKTOP-136JS7H>
2023-04-16 18:22:13 +0300

    change index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import readlineSync from 'readline-sync';
 
-const getRandomNumber = (min, max) => {
+RandomNumber = (min, max) => {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
@@ -14,8 +13,8 @@ const correctAnswer = () => {
 }
 
 const wrongAnswer = (c, w, name) => {
-    console.log(`"${w}" is wrong answer ;(. Correct answer was "${c}"`);
-    console.log("Let's try again, " + name);
+    console.log(`"${w}" is wrong answer ;(. Correct answer was "${c}".`);
+    console.log("Let's try again, " + name + "!");
 }
 
 const endGame = (i, name) => {
